0ca28d9ee04362e0e929128ea94c451dcea4725f
Michael L. Brock <mlbrock@users.noreply.github.com>
2024-09-09 21:08:32 +0100

    Add exception class support

---
+++ include/NatsWrapper/NatsExceptionStatus.hpp
@@ -0,0 +1,82 @@
+// ////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
+// MLB NatsWrapper Library Include File
+// ////////////////////////////////////////////////////////////////////////////
+/*
+   File Name         :  NatsExceptionStatus.hpp
+
+   File Description  :  Include file for the NatsExceptionStatus class.
+
+   Revision History  :  2024-09-10 --- Creation.
+                           Michael L. Brock
+
+      Copyright Michael L. Brock 2024.
+      Distributed under the Boost Software License, Version 1.0.
+      (See accompanying file LICENSE_1_0.txt or copy at
+      http://www.boost.org/LICENSE_1_0.txt)
+
+*/
+// ////////////////////////////////////////////////////////////////////////////
+
+#ifndef HH__MLB__NatsWrapper__NatsExceptionStatus_hpp__HH
+
+#define HH__MLB__NatsWrapper__NatsExceptionStatus_hpp__HH 1
+
+// ////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
+/**
+   \file NatsExceptionStatus.hpp
+
+   \brief   Main include file for the NatsExceptionStatus class.
+*/
+// ////////////////////////////////////////////////////////////////////////////
+
+// ////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
+// Required include files...
+// ////////////////////////////////////////////////////////////////////////////
+
+#include <Utility/Utility.hpp>
+
+#include <nats.h>
+
+#include <stdexcept>
+#include <string>
+
+// ////////////////////////////////////////////////////////////////////////////
+
+namespace MLB {
+
+namespace NatsWrapper {
+
+// ////////////////////////////////////////////////////////////////////////////
+class MB_LIB_EXCEPTION_CLASS(API_NATSWRAPPER) NatsExceptionStatus :
+	public std::runtime_error
+{
+public:
+	NatsExceptionStatus();
+	NatsExceptionStatus(const std::string &error_text);
+	NatsExceptionStatus(natsStatus nats_code, const char *func_name);
+
+	virtual ~NatsExceptionStatus();
+
+/*
+	virtual const char *what() noexcept;
+*/
+
+	natsStatus GetNatsStatus() const;
+
+private:
+	natsStatus nats_code_;
+
+	static std::string GetStatusString(natsStatus nats_code,
+		const char *func_name);
+};
+// ////////////////////////////////////////////////////////////////////////////
+
+} // namespace NatsWrapper
+
+} // namespace MLB
+
+#endif // #ifndef HH__MLB__NatsWrapper__NatsExceptionStatus_hpp__HH
+
